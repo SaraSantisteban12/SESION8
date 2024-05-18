94d62e1ff11fab140f6c72dd0c843ab41badf4ea
SaraSantisteban12 <166246482+SaraSantisteban12@users.noreply.github.com>
2024-05-18 14:01:39 -0500

    Add files via upload

---
+++ src/sesion8/entidades/SmartPhone.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sesion8.entidades;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class SmartPhone {
+    
+}
